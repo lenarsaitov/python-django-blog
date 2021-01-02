ed6c1ceb21a45e82fe7df3f32e92eaaa58952cd9
Lenar Saitov <lenarsaitov1@yandex.ru>
2021-01-03 02:36:52 +0300

    creating base

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+def posts_list(request):
+    n = ['Name1', 'Name2', 'Name3']
+    return render(request, 'blog/index.html', context={'names': n})
--- blogengine/urls.py
+++ blogengine/urls.py
@@ -15,10 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls import include
 
-from .views import hello
+# from .views import hello
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('blog/', hello)
+    path('blog/', include('blog.urls'))
 ]
