a65d1e69f29e5f6ed976a3c28b1eefe7b31fd9d7
Lenar Saitov <lenarsaitov1@yandex.ru>
2021-01-03 23:18:16 +0300

    class based views and use mixins

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 from .models import Post, Tag
+from django.views.generic import View
+from .utils import ObjectDetailMixin
 from django.http import HttpResponse
 # Create your views here.
 
@@ -7,15 +10,16 @@ def posts_list(request):
     posts = Post.objects.all()
     return render(request, 'blog/index.html', context={'posts': posts})
 
-def post_detail(request, slug):
-    post = Post.objects.get(slug__iexact = slug)
-    return render(request, 'blog/post_detail.html', context={'post': post})
+class PostDetail(ObjectDetailMixin, View):
+    model = Post
+    template = 'blog/post_detail.html'
+
+class TagDetail(ObjectDetailMixin, View):
+    model = Tag
+    template = 'blog/tag_detail.html'
 
 def tags_lists(request):
     tags = Tag.objects.all()
     return render(request, 'blog/tags_list.html', context={'tags':tags})
 
-def tag_detail(request, slug):
-    tag = Tag.objects.get(slug__iexact = slug)
-    return render(request, 'blog/tag_detail.html', context={'tag': tag})
 
