8e55c2176e4a305ab519ee7f77e15f4160b87b33
Lenar Saitov <lenarsaitov1@yandex.ru>
2021-01-07 01:48:26 +0300

    created create post form

--- blog/views.py
+++ blog/views.py
@@ -4,10 +4,8 @@ from django.shortcuts import redirect
 from .models import Post, Tag
 from django.views.generic import View
 from .utils import ObjectDetailMixin
-from django.http import HttpResponse
-from blog.forms import TagForm
+from blog.forms import TagForm, PostForm
 
-# Create your views here.
 def posts_list(request):
     posts = Post.objects.all()
     return render(request, 'blog/index.html', context={'posts': posts})
@@ -15,6 +13,21 @@ def posts_list(request):
 class PostDetail(ObjectDetailMixin, View):
     model = Post
     template = 'blog/post_detail.html'
+
+class PostCreate(View):
+    def get(self, request):
+        form = PostForm()
+        return render(request, 'blog/post_create.html', context={'form': form})
+
+    def post(self, request):
+        bound_form = PostForm(request.POST)
+
+        if bound_form.is_valid():
+            new_post = bound_form.save()
+            return redirect(new_post)
+        return render(request, 'blog/post_create.html', context={'form': bound_form})
+
+
 
 class TagDetail(ObjectDetailMixin, View):
     model = Tag
@@ -36,3 +49,4 @@ class TagCreate(View):
             new_tag = bound_form.save()
             return redirect(new_tag)
         return render(request, 'blog/tag_create.html', context={'form': bound_form})
+
