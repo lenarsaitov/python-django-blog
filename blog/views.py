d08ca8d448bdf2d0b7d8d57c5d12988ede47540f
Lenar Saitov <lenarsaitov1@yandex.ru>
2021-01-07 22:45:11 +0300

    added paginated navigation

--- blog/views.py
+++ blog/views.py
@@ -5,10 +5,34 @@ from django.views.generic import View
 from .utils import *
 from blog.forms import TagForm, PostForm
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.core.paginator import Paginator
 
 def posts_list(request):
     posts = Post.objects.all()
-    return render(request, 'blog/index.html', context={'posts': posts})
+    paginator = Paginator(posts, 4)
+
+    page_number = request.GET.get('page', 1)
+    page = paginator.get_page(page_number)
+
+    is_paginated = page.has_other_pages()
+
+    if page.has_previous():
+        previous_url = f"?page={page.previous_page_number()}"
+    else:
+        previous_url = ''
+
+    if page.has_next():
+        next_url = f"?page={page.next_page_number()}"
+    else:
+        next_url = ''
+
+    context = {
+        'posts': page,
+        'is_paginated': is_paginated,
+        'previous_url' : previous_url,
+        'next_url':next_url
+    }
+    return render(request, 'blog/index.html', context=context)
 
 class PostDetail(ObjectDetailMixin, View):
     model = Post
