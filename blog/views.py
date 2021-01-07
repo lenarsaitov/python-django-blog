c11f15fa54156528595970f55b6c0155c4a78f14
Lenar Saitov <lenarsaitov1@yandex.ru>
2021-01-07 21:30:46 +0300

    added restricting access to pages

--- blog/views.py
+++ blog/views.py
@@ -4,6 +4,7 @@ from .models import Post, Tag
 from django.views.generic import View
 from .utils import *
 from blog.forms import TagForm, PostForm
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 def posts_list(request):
     posts = Post.objects.all()
@@ -13,19 +14,22 @@ class PostDetail(ObjectDetailMixin, View):
     model = Post
     template = 'blog/post_detail.html'
 
-class PostCreate(ObjectCreateMixin, View):
+class PostCreate(LoginRequiredMixin, ObjectCreateMixin, View):
     form_model = PostForm
     template = 'blog/post_create.html'
+    raise_exception = True
 
-class PostUpdate(ObjectUpdateMixin, View):
+class PostUpdate(LoginRequiredMixin, ObjectUpdateMixin, View):
     model = Post
     model_form = PostForm
     template = 'blog/post_update_form.html'
+    raise_exception = True
 
-class PostDelete(ObjectDeleteMixin, View):
+class PostDelete(LoginRequiredMixin, ObjectDeleteMixin, View):
     model = Post
     template = 'blog/post_delete_form.html'
     redirect_url = 'posts_list_url'
+    raise_exception = True
 
 class TagDetail(ObjectDetailMixin, View):
     form_model = Tag
@@ -35,18 +39,21 @@ def tags_lists(request):
     tags = Tag.objects.all()
     return render(request, 'blog/tags_list.html', context={'tags':tags})
 
-class TagCreate(ObjectCreateMixin, View):
+class TagCreate(LoginRequiredMixin, ObjectCreateMixin, View):
     model = TagForm
     template = 'blog/tag_create.html'
+    raise_exception = True
 
-class TagUpdate(ObjectUpdateMixin, View):
+class TagUpdate(LoginRequiredMixin, ObjectUpdateMixin, View):
     model = Tag
     model_form = TagForm
     template = 'blog/tag_update_form.html'
+    raise_exception = True
 
-class TagDelete(ObjectDeleteMixin, View):
+class TagDelete(LoginRequiredMixin, ObjectDeleteMixin, View):
     model = Tag
     template = 'blog/tag_delete_form.html'
     redirect_url = 'tags_list_url'
+    raise_exception = True
 
 
